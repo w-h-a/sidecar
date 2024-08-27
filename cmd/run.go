f529cbd60ba237884c5479c7c6ed8b594136eef1
w-h-a <wesley.herb.anderson@gmail.com>
2024-08-26 17:14:40 -0700

    feat: list

--- cmd/run.go
+++ cmd/run.go
@@ -140,6 +140,7 @@ func run(ctx *cli.Context) {
 
 	router.Methods("POST").Path("/publish").HandlerFunc(publish.Handle)
 	router.Methods("POST").Path("/state/{storeId}").HandlerFunc(state.HandlePost)
+	router.Methods("GET").Path("/state/{storeId}").HandlerFunc(state.HandleList)
 	router.Methods("GET").Path("/state/{storeId}/{key}").HandlerFunc(state.HandleGet)
 
 	opts := []api.ApiOption{
