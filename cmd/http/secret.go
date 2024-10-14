57529a1ecfbe0589ce3622cf39d7603dba8c5202
w-h-a <wesley.herb.anderson@gmail.com>
2024-10-13 17:03:17 -0700

    fix: tracer

--- cmd/http/secret.go
+++ cmd/http/secret.go
@@ -29,9 +29,9 @@ func (h *secretHandler) HandleGet(w gohttp.ResponseWriter, r *gohttp.Request) {
 
 	ctx := metadatautils.RequestToContext(r)
 
-	newCtx := h.tracer.Start(ctx, "secretHandler")
+	newCtx, spanId := h.tracer.Start(ctx, "secretHandler")
 
-	h.tracer.AddMetadata(map[string]string{
+	h.tracer.AddMetadata(spanId, map[string]string{
 		"secretId": secretId,
 		"key":      key,
 	})
@@ -39,19 +39,19 @@ func (h *secretHandler) HandleGet(w gohttp.ResponseWriter, r *gohttp.Request) {
 	secret, err := h.service.ReadFromSecretStore(newCtx, secretId, key)
 	if err != nil && err == sidecar.ErrComponentNotFound {
 		// TODO: update span status
-		h.tracer.Finish()
+		h.tracer.Finish(spanId)
 		httputils.ErrResponse(w, errorutils.NotFound("sidecar", "%s: %s", err.Error(), secretId))
 		return
 	} else if err != nil {
 		// TODO: update span status
-		h.tracer.Finish()
+		h.tracer.Finish(spanId)
 		httputils.ErrResponse(w, errorutils.InternalServerError("sidecar", "failed to retrieve secret from store %s and key %s: %v", secretId, key, err))
 		return
 	}
 
 	// TODO: update span status
 
-	h.tracer.Finish()
+	h.tracer.Finish(spanId)
 
 	httputils.OkResponse(w, secret)
 }
